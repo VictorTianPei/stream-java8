9ef0ff7f9a0c40716824ecdd53df67b75b16ba51
tianpei <tianpei@mail.taiji.com.cn>
2017-04-01 17:10:33 +0800

    10个台阶，每次可走1,2,3步的走法

--- src/main/java/FibonacciSupplier.java
+++ src/main/java/FibonacciSupplier.java
@@ -4,7 +4,7 @@ import java.util.function.Supplier;
  * Created by qiuzhanghua on 2017/2/7.
  */
 public class FibonacciSupplier implements Supplier<Long> {
-  private long previous = 1;
+/*  private long previous = 1;
   private long current = 2;
 
   @Override
@@ -14,5 +14,20 @@ public class FibonacciSupplier implements Supplier<Long> {
     previous = current;
     current = next;
     return p;
+  }*/
+	private long a = 1;
+	private long b = 2;
+	private long c = 4;
+	
+  @Override
+  public Long get() {
+    long p1 = a;
+    long p2 = b;
+    long p3 = c;
+    long next=p1+p2+p3;
+    a=p2;
+    b=p3;
+    c=next;
+    return p1;
   }
 }
